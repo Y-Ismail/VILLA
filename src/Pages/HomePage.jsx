1b6b2bd56c5616ba8123078f9fe8ea788dfe9b3d
Oyama <godstimeoyama082@gmail.com>
2024-09-11 08:27:59 +0800

    ...

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,18 @@
+
+
+function HomePage() {
+  return (
+    <>
+      <header className="h-[10vh] border-b-[1.4px] border-black grid place-content-center ">
+        Header
+      </header>
+
+      <main className="min-h-[80vh] grid place-content-center border-b-[1.4px] border-black  ">
+        Body
+      </main>
+      <footer className="h-[10vh] grid  place-content-center">footer</footer>
+    </>
+  );
+}
+
+export default HomePage
